e0606bd7d22a64e50776063467486e31504cac90
Igor Mukhin <igor.mukhin@gmail.com>
2019-12-24 13:41:40 +0200

    Added: Max length constraint to quickpay order prefix

--- src/Form/Type/QuickPayGatewayConfigurationType.php
+++ src/Form/Type/QuickPayGatewayConfigurationType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class QuickPayGatewayConfigurationType extends AbstractType
@@ -58,6 +59,13 @@ class QuickPayGatewayConfigurationType extends AbstractType
             ->add('order_prefix', TextType::class, [
                 'label' => 'setono_sylius_quickpay.form.gateway_configuration.quickpay.order_prefix',
                 'required' => false,
+                'constraints' => [
+                    new Length([
+                        'maxMessage' => 'setono_sylius_quickpay.form.gateway_configuration.quickpay.order_prefix.max_length',
+                        'max' => 11,
+                        'groups' => 'sylius',
+                    ]),
+                ],
             ])
             ->add('payment_methods', TextType::class, [
                 'label' => 'setono_sylius_quickpay.form.gateway_configuration.quickpay.payment_methods',
