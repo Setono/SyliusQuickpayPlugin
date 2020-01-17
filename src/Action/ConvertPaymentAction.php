acd56718031d33e957d13ed8e89c5be448519f9d
Joachim Løvgaard <joachim@loevgaard.dk>
2020-01-17 15:45:09 +0100

    Removed `assert` and added `Assert` lib instead

--- src/Action/ConvertPaymentAction.php
+++ src/Action/ConvertPaymentAction.php
@@ -24,6 +24,7 @@ use Sylius\Component\Core\Model\OrderItemInterface;
 use Sylius\Component\Core\Model\PaymentInterface as SyliusPaymentInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 use Symfony\Component\Intl\Countries;
+use Webmozart\Assert\Assert;
 
 class ConvertPaymentAction implements ActionInterface, ApiAwareInterface, GatewayAwareInterface
 {
@@ -71,19 +72,19 @@ class ConvertPaymentAction implements ActionInterface, ApiAwareInterface, Gatewa
 
         $identity = $token->getDetails();
         $syliusPayment = $this->payum->getStorage($identity->getClass())->find($identity);
-        assert($syliusPayment instanceof SyliusPaymentInterface);
+        Assert::isInstanceOf($syliusPayment, SyliusPaymentInterface::class);
 
         $order = $syliusPayment->getOrder();
-        assert($order instanceof OrderInterface);
+        Assert::isInstanceOf($order, OrderInterface::class);
 
         $customer = $order->getCustomer();
-        assert($customer instanceof CustomerInterface);
+        Assert::isInstanceOf($customer, CustomerInterface::class);
 
         $shippingAddress = $order->getShippingAddress();
-        assert($shippingAddress instanceof AddressInterface);
+        Assert::isInstanceOf($shippingAddress, AddressInterface::class);
 
         $billingAddress = $order->getBillingAddress();
-        assert($billingAddress instanceof AddressInterface);
+        Assert::isInstanceOf($billingAddress, AddressInterface::class);
 
         $details['shipping_address'] = $this->convertAddress($shippingAddress, $customer);
         $details['invoice_address'] = $this->convertAddress($billingAddress, $customer);
@@ -97,7 +98,7 @@ class ConvertPaymentAction implements ActionInterface, ApiAwareInterface, Gatewa
     protected function convertAddress(AddressInterface $address, CustomerInterface $customer): array
     {
         $countryCode = $address->getCountryCode();
-        assert(null !== $countryCode);
+        Assert::notNull($countryCode);
 
         $details = [];
         $details['name'] = sprintf(
@@ -123,7 +124,7 @@ class ConvertPaymentAction implements ActionInterface, ApiAwareInterface, Gatewa
     {
         return $items->map(function (OrderItemInterface $orderItem): array {
             $variant = $orderItem->getVariant();
-            assert($variant instanceof ProductVariantInterface);
+            Assert::isInstanceOf($variant, ProductVariantInterface::class);
 
             return [
                 'qty' => $orderItem->getQuantity(),
@@ -145,6 +146,6 @@ class ConvertPaymentAction implements ActionInterface, ApiAwareInterface, Gatewa
             $request instanceof Convert &&
             $request->getSource() instanceof PayumPaymentInterface &&
             $request->getTo() === 'array'
-            ;
+        ;
     }
 }
